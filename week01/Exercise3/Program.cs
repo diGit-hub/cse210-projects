affaa6f9ac52adfb8fcee7b5720fb512a292c780
diGit-hub <gabrielsud.m.s@gmail.com>
2026-01-10 23:30:06 -0300

    Complete exercise 03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool keepGoing = true;
+        while (keepGoing)
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = 0;
+            int tries = 0;
+            Console.WriteLine("I'm imagining a number. Can you guess it?");
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                tries++;
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {tries} tries!");
+                }
+            } while (magicNumber != guess);
+
+            Console.Write("Do you want to play again? (y/N) ");
+            string answer = Console.ReadLine();
+            if (answer != "y")
+            {
+                keepGoing = false;
+            }
+        }
+        Console.WriteLine("Bye!");
     }
 }
