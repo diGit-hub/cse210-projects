d9b87fdd791392dcaf3f11dfe37c2da012127af7
diGit-hub <gabrielsud.m.s@gmail.com>
2026-02-17 21:30:29 -0300

    Complete Polymorphism Learning Activity (Week 06)

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square shape1 = new Square("red", 24);
+        Rectangle shape2 = new Rectangle("blue", 23, 54);
+        Circle shape3 = new Circle("Alice Blue", 32);
+
+        List<Shape> list = new List<Shape>();
+        list.Add(shape1);
+        list.Add(shape2);
+        list.Add(shape3);
+
+        foreach(Shape item in list)
+        {
+            Console.WriteLine(item.GetColor());
+            Console.WriteLine(item.GetArea());  
+        }
     }
 }
