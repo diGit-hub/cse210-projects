fef04098bcd0e5903b7015d58ee3baf08c66140c
diGit-hub <gabrielsud.m.s@gmail.com>
2026-02-19 19:12:26 -0300

    Complete Exercise Tracking Program (Week 07)

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+        activities.Add(new Running(new DateTime(2026, 1, 4), 30, 3.0));
+        activities.Add(new Cycling(new DateTime(2026, 2, 5), 45, 20.0));
+        activities.Add(new Swimming(new DateTime(2026, 3, 6), 40, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
